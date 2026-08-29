783cf8dbdfeae7122324f7fd848eff59bbf6dff0
keto178 <codingk178@gmail.com>
2026-08-29 09:57:05 -0700

    Fix metadata base URL

--- app/data/projects.js
+++ app/data/projects.js
@@ -1,6 +1,6 @@
 export const projects = [
     {
-        image: '/fudhub',
+        image: '/fudhub.jpg',
         title: 'FoodHub \u2014 React Food Ordering Web App',
         description: 'A modern, responsive food ordering application built with React and Vite, featuring meal browsing, cart management, user authentication, and an account dashboard to review and track orders.',
         tech: ['React', 'JavaScript', 'Vite'],
@@ -8,7 +8,7 @@ export const projects = [
         github: 'https://github.com/keto178/FoodHub.git',
     },
     {
-        image: '/vipe',
+        image: '/vipe.jpg',
         title: 'VibeStore — Modern E-Commerce Platform with Admin Dashboard',
         description: 'A modern, responsive e-commerce platform built with React, featuring product discovery with filtering & search, cart and checkout flow, user authentication, and a complete admin dashboard to manage products, orders, and inventory.',
         tech: ['React.js', 'JavaScript','React Router','Context API','css3', 'Vercel'],
